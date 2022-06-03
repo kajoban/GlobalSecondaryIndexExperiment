7495b12d669899f2cb9d4fe8143721a7d99fa4f9
Kajoban Kuhaparan <kajoban.kuhaparan@ssense.com>
2022-06-03 16:27:14 -0400

    remove count data

--- countData/countData.js
+++
@@ -1,9 +0,0 @@
-var AWS = require('aws-sdk');
-
-var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
-
-exports.handler = async (event) => {
-    const tableName = "kajoban-order-data-table-1"
-    const data = await ddb.scan({ Select: "COUNT", TableName: tableName }).promise();
-    return `there are ${data.Count} items in ${tableName}`;
-};
